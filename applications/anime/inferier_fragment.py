2c8d39dd664bec7c67374333ba0f5fbf66bf3e2c
Shunsuke Kimura <pbrehpuum@gmail.com>
2025-07-29 12:05:51 +0900

    fix bug

--- applications/anime/inferier_fragment.py
+++ applications/anime/inferier_fragment.py
@@ -49,7 +49,7 @@ class AnimeInferOp(Operator):
 
         input_array = cp.expand_dims(input_array, axis=0)
 
-        if model_path.endswith("_16.onnx"):
+        if self.model_path.endswith("_16.onnx"):
             result  = self.session.run([self.output_name], {self.input_name: cp.asnumpy(input_array).astype(np.float16) })
         else:
             result  = self.session.run([self.output_name], {self.input_name: cp.asnumpy(input_array).astype(np.float32) })
